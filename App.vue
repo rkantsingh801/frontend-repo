acc5bb7d2bac93fb7621303a1566f2667a47236c
Ramakant <ramakant.php@gmail.com>
2025-04-10 22:15:05 +0530

    frontend repo final commit

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Users</h1>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.FirstName }} {{ user.LastName }} ({{ user.Email }})
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        users: [],
+      };
+    },
+    mounted() {
+      axios.get('http://localhost:3000/users').then((response) => {
+        this.users = response.data;
+      });
+    },
+  };
+  </script>
